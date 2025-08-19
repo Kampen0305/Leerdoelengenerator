60d3c3d2bac381a05cd51d703b5e879acb90ef6b
Kampen0305 <edwinspielhagen@gmail.com>
2025-08-19 09:04:10 +0200

    feat: add reusable promo footer

--- Leerdoelengenerator-main/src/main.tsx
+++ Leerdoelengenerator-main/src/main.tsx
@@ -2,13 +2,19 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import About from './pages/About.tsx';
+import Layout from './components/Layout.tsx';
 import './index.css';
 
 const rootElement = document.getElementById('root')!;
 
 function Router() {
   const path = window.location.pathname;
-  return path === '/over' ? <About /> : <App />;
+  const Page = path === '/over' ? About : App;
+  return (
+    <Layout>
+      <Page />
+    </Layout>
+  );
 }
 
 createRoot(rootElement).render(
