579570643dc0ac736798c7a49eb9b44575a8ef5b
Kampen0305 <edwinspielhagen@gmail.com>
2025-09-15 20:59:30 +0200

    Sanitize document title to remove DigitEd suffix

--- Leerdoelengenerator-main/src/components/RouteTracker.tsx
+++ Leerdoelengenerator-main/src/components/RouteTracker.tsx
@@ -2,12 +2,22 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { sendPageView } from '@/lib/ga';
 
+const DIGITED_SUFFIX = /\s*\|?\s*DigitEd\s*$/i;
+const DEFAULT_TITLE = 'AI Leerdoelenmaker';
+
 export default function RouteTracker() {
   const location = useLocation();
 
   useEffect(() => {
+    const currentTitle = document.title;
+    const sanitizedTitle = currentTitle.replace(DIGITED_SUFFIX, '').trim() || DEFAULT_TITLE;
+
+    if (sanitizedTitle !== currentTitle) {
+      document.title = sanitizedTitle;
+    }
+
     // Consent-status bepaalt vanzelf of dit cookieless of enhanced is
-    sendPageView(location.pathname, document.title);
+    sendPageView(location.pathname, sanitizedTitle);
   }, [location]);
 
   return null;
